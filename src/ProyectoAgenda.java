0a90c637e452eefddb2dbb4e2eca813444fb9fc3
Gil Pablo <gilpablo2000.gpbp@gmail.com>
2020-11-27 14:28:46 +0100

    case 2

--- src/ProyectoAgenda.java
+++ src/ProyectoAgenda.java
@@ -8,7 +8,8 @@ public class ProyectoAgenda {
 		Scanner leer = new Scanner(System.in);
 
 		do {
-
+			
+			System.out.println("---------");
 			System.out.println("AGENDA");
 			System.out.println("---------");
 			System.out.println("1-Anadir contactos");
@@ -51,7 +52,28 @@ public class ProyectoAgenda {
 				nombre = leer.next();
 				System.out.println("Dime un numero de teléfono");
 				telefono = leer.next();
-
+				System.out.println("Contacto guardado, gracias.");	
+				System.out.println(nombre);
+				break;
+			case 2:
+				
+				String nombreBuscar;
+				System.out.println("Dime el nombre que quieres buscar");
+				nombreBuscar=leer.next();
+				
+				//problemas inicilizar variable "nombre"
+				
+				
+				break;
+			case 3:
+				
+			case 4:
+				
+			case 5:
+				
+			case 6:
+				System.out.println("Gracias por su atención, ¡Adiós!");
+				
 			}
 
 		}
