c39aaa3418b99245d6bec8f4d6cf4a38764d9c06
Gil Pablo <gilpablo2000.gpbp@gmail.com>
2021-02-08 12:38:53 +0100

    nuevo paquete-IODatos

--- src/IODatos/IODatos.java
+++ src/IODatos/IODatos.java
@@ -1,5 +1,25 @@
 package IODatos;
 
-public class IODatos {
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class IODatos {
+	private File fichero;
+	private FileWriter fw;
+	private PrintWriter pw;
+	private FileReader fr;
+	private Scanner leer;
+	
+	
+	public IODatos() {
+		fichero null;
+		fw null;
+		pw null;
+		fr null;
+		leer null;
+	}
+	
 }
